57b0aa4934657be4279d00475d19facaa68f2de1
Yusuf Adedeji <adedejiyusuf26@gmail.com>
2024-09-09 12:48:35 +0100

    Added changes

--- pages/api/leaderboard.ts
+++ pages/api/leaderboard.ts
@@ -83,316 +83,217 @@ export default async function handler(
 
   let formattedLeaderboard = [];
 
-  if (sortBy && sortBy !== "totalPoints") {
-    const leaderboardData = await prisma.userChallenge.findMany({
-      where: {
-        challengeId,
-        OR: searchQuery
-          ? [
-              {
-                user: {
-                  firstName: {
-                    contains: searchQuery,
-                    mode: "insensitive",
-                  },
+  const leaderboardData = await prisma.userChallenge.findMany({
+    where: {
+      challengeId,
+      OR: searchQuery
+        ? [
+            {
+              user: {
+                firstName: {
+                  contains: searchQuery,
+                  mode: "insensitive",
                 },
               },
-              {
-                user: {
-                  lastName: {
-                    contains: searchQuery,
-                    mode: "insensitive",
-                  },
+            },
+            {
+              user: {
+                lastName: {
+                  contains: searchQuery,
+                  mode: "insensitive",
                 },
               },
-            ]
-          : undefined,
-      },
-      include: {
-        user: {
-          select: {
-            firstName: true,
-            lastName: true,
-          },
-        },
-        TaskSubmission: {
-          select: {
-            upworkOutreach: true,
-            socialMediaPosts: true,
-            socialMediaEngagements: true,
-            jobApplications: true,
-            localOutreach: true,
-            intlOutreach: true,
-            ecommerceDeliveredOrders: true,
-            noOfClients: true,
-            earningsInDollars: true,
-            // opportunities: true,
-            // jobSecured: true,
-            // earningsInNaira: true,
-            // socialGroupPost: true,
-          },
+            },
+          ]
+        : undefined,
+    },
+    include: {
+      user: {
+        select: {
+          firstName: true,
+          lastName: true,
         },
       },
-    });
-
-    // Step 2: Aggregate and sort by upworkOutreach
-    formattedLeaderboard = leaderboardData.map((userChallenge) => {
-      const aggregatedTasks = userChallenge.TaskSubmission.reduce(
-        (acc, task) => {
-          acc.upworkOutreach += task.upworkOutreach;
-          acc.socialMediaPosts += task.socialMediaPosts;
-          acc.socialMediaEngagements += task.socialMediaEngagements;
-          acc.jobApplications += task.jobApplications;
-          acc.localOutreach += task.localOutreach;
-          acc.intlOutreach += task.intlOutreach;
-          acc.ecommerceDeliveredOrders += task.ecommerceDeliveredOrders;
-          acc.noOfClients += task.noOfClients;
-          acc.earningsInDollars += task.earningsInDollars;
-          // acc.opportunities += task.opportunities;
-          // acc.jobSecured += task.jobSecured;
-          // acc.earningsInNaira += task.earningsInNaira;
-          // acc.socialGroupPost += task.socialGroupPost;
-          return acc;
+      TaskSubmission: {
+        select: {
+          upworkOutreach: true,
+          socialMediaPosts: true,
+          socialMediaEngagements: true,
+          jobApplications: true,
+          localOutreach: true,
+          intlOutreach: true,
+          ecommerceDeliveredOrders: true,
+          noOfClients: true,
+          earningsInDollars: true,
+          opportunities: true,
+          jobSecured: true,
+          earningsInNaira: true,
+          socialGroupPost: true,
         },
-        {
-          upworkOutreach: 0,
-          socialMediaPosts: 0,
-          socialMediaEngagements: 0,
-          jobApplications: 0,
-          localOutreach: 0,
-          intlOutreach: 0,
-          ecommerceDeliveredOrders: 0,
-          noOfClients: 0,
-          earningsInDollars: 0,
-          // opportunities: 0,
-          // jobSecured: 0,
-          // earningsInNaira: 0,
-          // socialGroupPost: 0,
-        }
-      );
+      },
+    },
+  });
 
-      return {
-        user: userChallenge.user,
-        totalPoints: userChallenge.totalPoints,
-        aggregatedTasks,
-      };
-    });
-
-    switch (sortBy) {
-      case "upworkOutreach":
-        console.log("Log>>>", sortBy, sortDir);
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.upworkOutreach -
-              a.aggregatedTasks.upworkOutreach
-            : a.aggregatedTasks.upworkOutreach -
-              b.aggregatedTasks.upworkOutreach
-        );
-        break;
-      case "socialMediaPosts":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.socialMediaPosts -
-              a.aggregatedTasks.socialMediaPosts
-            : a.aggregatedTasks.socialMediaPosts -
-              b.aggregatedTasks.socialMediaPosts
-        );
-        break;
-
-      case "socialMediaEngagements":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.socialMediaEngagements -
-              a.aggregatedTasks.socialMediaEngagements
-            : a.aggregatedTasks.socialMediaEngagements -
-              b.aggregatedTasks.socialMediaEngagements
-        );
-        break;
-      case "jobApplications":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.jobApplications -
-              a.aggregatedTasks.jobApplications
-            : a.aggregatedTasks.jobApplications -
-              b.aggregatedTasks.jobApplications
-        );
-        break;
-      case "localOutreach":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.localOutreach - a.aggregatedTasks.localOutreach
-            : a.aggregatedTasks.localOutreach - b.aggregatedTasks.localOutreach
-        );
-        break;
-      case "intlOutreach":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.intlOutreach - a.aggregatedTasks.intlOutreach
-            : a.aggregatedTasks.intlOutreach - b.aggregatedTasks.intlOutreach
-        );
-        break;
-      case "ecommerceDeliveredOrders":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.ecommerceDeliveredOrders -
-              a.aggregatedTasks.ecommerceDeliveredOrders
-            : a.aggregatedTasks.ecommerceDeliveredOrders -
-              b.aggregatedTasks.ecommerceDeliveredOrders
-        );
-        break;
-      case "noOfClients":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.noOfClients - a.aggregatedTasks.noOfClients
-            : a.aggregatedTasks.noOfClients - b.aggregatedTasks.noOfClients
-        );
-        break;
-      case "earningsInDollars":
-        formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-          sortDir === "desc"
-            ? b.aggregatedTasks.earningsInDollars -
-              a.aggregatedTasks.earningsInDollars
-            : a.aggregatedTasks.earningsInDollars -
-              b.aggregatedTasks.earningsInDollars
-        );
-      // case "opportunities":
-      //   formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-      //     sortDir === "desc"
-      //       ? b.aggregatedTasks.opportunities - a.aggregatedTasks.opportunities
-      //       : a.aggregatedTasks.opportunities - b.aggregatedTasks.opportunities
-      //   );
-      // case "jobSecured":
-      //   formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-      //     sortDir === "desc"
-      //       ? b.aggregatedTasks.jobSecured - a.aggregatedTasks.jobSecured
-      //       : a.aggregatedTasks.jobSecured - b.aggregatedTasks.jobSecured
-      //   );
-      // case "earningsInNaira":
-      //   formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-      //     sortDir === "desc"
-      //       ? b.aggregatedTasks.earningsInNaira -
-      //         a.aggregatedTasks.earningsInNaira
-      //       : a.aggregatedTasks.earningsInNaira -
-      //         b.aggregatedTasks.earningsInNaira
-      //   );
-      //   break;
-      // case "socialGroupPost":
-      //   formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
-      //     sortDir === "desc"
-      //       ? b.aggregatedTasks.socialGroupPost -
-      //         a.aggregatedTasks.socialGroupPost
-      //       : a.aggregatedTasks.socialGroupPost -
-      //         b.aggregatedTasks.socialGroupPost
-      //   );
-        break;
-      default:
-        break;
-    }
-
-    // Step 3: Apply pagination
-    formattedLeaderboard = formattedLeaderboard.slice(
-      Number(page) * Number(pageSize),
-      (Number(page) + 1) * Number(pageSize)
+  // Step 2: Aggregate and sort by upworkOutreach
+  formattedLeaderboard = leaderboardData.map((userChallenge) => {
+    const aggregatedTasks = userChallenge.TaskSubmission.reduce(
+      (acc, task) => {
+        acc.upworkOutreach += task.upworkOutreach;
+        acc.socialMediaPosts += task.socialMediaPosts;
+        acc.socialMediaEngagements += task.socialMediaEngagements;
+        acc.jobApplications += task.jobApplications;
+        acc.localOutreach += task.localOutreach;
+        acc.intlOutreach += task.intlOutreach;
+        acc.ecommerceDeliveredOrders += task.ecommerceDeliveredOrders;
+        acc.noOfClients += task.noOfClients;
+        acc.earningsInDollars += task.earningsInDollars;
+        acc.opportunities += task.opportunities;
+        acc.jobSecured += task.jobSecured;
+        acc.earningsInNaira += task.earningsInNaira;
+        acc.socialGroupPost += task.socialGroupPost;
+        return acc;
+      },
+      {
+        upworkOutreach: 0,
+        socialMediaPosts: 0,
+        socialMediaEngagements: 0,
+        jobApplications: 0,
+        localOutreach: 0,
+        intlOutreach: 0,
+        ecommerceDeliveredOrders: 0,
+        noOfClients: 0,
+        earningsInDollars: 0,
+        opportunities: 0,
+        jobSecured: 0,
+        earningsInNaira: 0,
+        socialGroupPost: 0,
+      }
     );
-  } else {
-    const leaderboard = await prisma.userChallenge.findMany({
-      where: {
-        challengeId,
-        OR: searchQuery
-          ? [
-              {
-                user: {
-                  firstName: {
-                    contains: searchQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-              {
-                user: {
-                  lastName: {
-                    contains: searchQuery,
-                    mode: "insensitive",
-                  },
-                },
-              },
-            ]
-          : undefined,
-      },
-      include: {
-        user: {
-          select: {
-            firstName: true,
-            lastName: true,
-          },
-        },
-        TaskSubmission: {
-          select: {
-            upworkOutreach: true,
-            socialMediaPosts: true,
-            socialMediaEngagements: true,
-            jobApplications: true,
-            localOutreach: true,
-            intlOutreach: true,
-            ecommerceDeliveredOrders: true,
-            noOfClients: true,
-            earningsInDollars: true,
-            // opportunities: true,
-            // jobSecured: true,
-            // earningsInNaira: true,
-            // socialGroupPost: true,
-          },
-        },
-      },
-      orderBy: {
-        totalPoints: sortDir,
-      },
-      skip: Number(page) * Number(pageSize),
-      take: Number(pageSize),
-    });
 
-    formattedLeaderboard = leaderboard.map((userChallenge) => {
-      const aggregatedTasks = userChallenge.TaskSubmission.reduce(
-        (acc, task) => {
-          acc.upworkOutreach += task.upworkOutreach;
-          acc.socialMediaPosts += task.socialMediaPosts;
-          acc.socialMediaEngagements += task.socialMediaEngagements;
-          acc.jobApplications += task.jobApplications;
-          acc.localOutreach += task.localOutreach;
-          acc.intlOutreach += task.intlOutreach;
-          acc.ecommerceDeliveredOrders += task.ecommerceDeliveredOrders;
-          acc.noOfClients += task.noOfClients;
-          acc.earningsInDollars += task.earningsInDollars;
-          // acc.opportunities += task.opportunities;
-          // acc.jobSecured += task.jobSecured;
-          // acc.earningsInNaira += task.earningsInNaira;
-          // acc.socialGroupPost += task.socialGroupPost;
-          return acc;
-        },
-        {
-          upworkOutreach: 0,
-          socialMediaPosts: 0,
-          socialMediaEngagements: 0,
-          jobApplications: 0,
-          localOutreach: 0,
-          intlOutreach: 0,
-          ecommerceDeliveredOrders: 0,
-          noOfClients: 0,
-          earningsInDollars: 0,
-          // opportunities: 0,
-          // jobSecured: 0,
-          // earningsInNaira: 0,
-          // socialGroupPost: 0,
-        }
-      );
+    return {
+      user: userChallenge.user,
+      totalPoints: userChallenge.totalPoints,
+      aggregatedTasks,
+    };
+  });
 
-      return {
-        user: userChallenge.user,
-        totalPoints: userChallenge.totalPoints,
-        aggregatedTasks,
-      };
-    });
+  if (!sortBy) {
+    sortBy = "earningsInDollars";
   }
+
+  switch (sortBy) {
+    case "upworkOutreach":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.upworkOutreach - a.aggregatedTasks.upworkOutreach
+          : a.aggregatedTasks.upworkOutreach - b.aggregatedTasks.upworkOutreach
+      );
+      break;
+    case "socialMediaPosts":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.socialMediaPosts -
+            a.aggregatedTasks.socialMediaPosts
+          : a.aggregatedTasks.socialMediaPosts -
+            b.aggregatedTasks.socialMediaPosts
+      );
+      break;
+
+    case "socialMediaEngagements":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.socialMediaEngagements -
+            a.aggregatedTasks.socialMediaEngagements
+          : a.aggregatedTasks.socialMediaEngagements -
+            b.aggregatedTasks.socialMediaEngagements
+      );
+      break;
+    case "jobApplications":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.jobApplications -
+            a.aggregatedTasks.jobApplications
+          : a.aggregatedTasks.jobApplications -
+            b.aggregatedTasks.jobApplications
+      );
+      break;
+    case "localOutreach":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.localOutreach - a.aggregatedTasks.localOutreach
+          : a.aggregatedTasks.localOutreach - b.aggregatedTasks.localOutreach
+      );
+      break;
+    case "intlOutreach":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.intlOutreach - a.aggregatedTasks.intlOutreach
+          : a.aggregatedTasks.intlOutreach - b.aggregatedTasks.intlOutreach
+      );
+      break;
+    case "ecommerceDeliveredOrders":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.ecommerceDeliveredOrders -
+            a.aggregatedTasks.ecommerceDeliveredOrders
+          : a.aggregatedTasks.ecommerceDeliveredOrders -
+            b.aggregatedTasks.ecommerceDeliveredOrders
+      );
+      break;
+    case "noOfClients":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.noOfClients - a.aggregatedTasks.noOfClients
+          : a.aggregatedTasks.noOfClients - b.aggregatedTasks.noOfClients
+      );
+      break;
+    case "earningsInDollars":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.earningsInDollars -
+            a.aggregatedTasks.earningsInDollars
+          : a.aggregatedTasks.earningsInDollars -
+            b.aggregatedTasks.earningsInDollars
+      );
+    case "opportunities":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.opportunities - a.aggregatedTasks.opportunities
+          : a.aggregatedTasks.opportunities - b.aggregatedTasks.opportunities
+      );
+    case "jobSecured":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.jobSecured - a.aggregatedTasks.jobSecured
+          : a.aggregatedTasks.jobSecured - b.aggregatedTasks.jobSecured
+      );
+    case "earningsInNaira":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.earningsInNaira -
+            a.aggregatedTasks.earningsInNaira
+          : a.aggregatedTasks.earningsInNaira -
+            b.aggregatedTasks.earningsInNaira
+      );
+      break;
+    case "socialGroupPost":
+      formattedLeaderboard = formattedLeaderboard.sort((a, b) =>
+        sortDir === "desc"
+          ? b.aggregatedTasks.socialGroupPost -
+            a.aggregatedTasks.socialGroupPost
+          : a.aggregatedTasks.socialGroupPost -
+            b.aggregatedTasks.socialGroupPost
+      );
+      break;
+    default:
+      break;
+  }
+
+  // Step 3: Apply pagination
+  formattedLeaderboard = formattedLeaderboard.slice(
+    Number(page) * Number(pageSize),
+    (Number(page) + 1) * Number(pageSize)
+  );
 
   const totalUserChallenges = await prisma.userChallenge.count({
     where: {
